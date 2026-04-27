d7c1aeb2fb246c493a3c924221b91857264ea87f
Huynh Duc Dung <dunghd.it@gmail.com>
2026-04-27 14:51:09 +0800

    fix: rename extension to whichkey-next
    The name 'whichkey' is already taken by the original VSpaceCode publisher
    in the VSCode Marketplace. Rename to 'whichkey-next' to allow publishing.
    
    Also update extensionId and README badges to match the new name.

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const extensionId = "jellydn.whichkey";
+export const extensionId = "jellydn.whichkey-next";
 export const contributePrefix = "whichkey";
 export enum ConfigKey {
   Delay = "delay",
